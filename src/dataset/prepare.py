a47eff7b3a9eee4ea0e6bafcf0deb7b9533b18cd
Mattytomo365 <matty.tom@icloud.com>
2026-01-26 16:37:11 +0000

    data preparation implemented (further contributions to #2)

---
+++ src/dataset/prepare.py
@@ -0,0 +1,14 @@
+from .load_save import load_csv
+'''
+Joins exogenous factor datasets to sales datasets by date
+Full, complete and well-explained data
+'''
+
+def merge_data(sales):
+    weather = load_csv('./data/weather_daily.csv')
+    holidays = load_csv('./data/holidays.csv')
+    events = load_csv('./data/events.csv')
+    merged = sales.merge(weather, on="date", how="left")  # left join keeps only sales dates
+    merged = merged.merge(holidays, on="date", how="left")
+    merged = merged.merge(events, on="date", how="left")
+    return merged
